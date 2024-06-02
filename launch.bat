e32dfb1b9dc142cdbf117ef22faca6d8b0df652a
Oliver-Lief <lief53638332@yeah.net>
2024-06-02 21:35:09 +0800

    增加文本长度显示

---
+++ launch.bat
@@ -0,0 +1,2 @@
+I:/python310/python.exe c:/Users/RF/Desktop/ChatTTS/webui.py
+pause
--- webui.py
+++ webui.py
@@ -14,9 +14,17 @@ def generate_seed():
 
 
 def generate_audio(
-    text, temperature, top_P, top_K, audio_seed_input, text_seed_input, refine_text_flag
+    text,
+    temperature,
+    top_P,
+    top_K,
+    audio_seed_input,
+    text_seed_input,
+    refine_text_flag,
+    par,
 ):
-    cut_word_num = 150
+    # on_text_change(total_text_number_box, text)
+    cut_word_num = 120
 
     torch.manual_seed(audio_seed_input)
     rand_spk = chat.sample_random_speaker()
@@ -38,13 +46,13 @@ def generate_audio(
     #     "prompt": f"[oral_{num_oral}][laugh_{num_laugh}][break_{num_break}]"
     # }
 
-    params_refine_text = {"prompt": "[oral_2][laugh_0][break_6]"}
+    params_refine_text = {"prompt": par}
 
     torch.manual_seed(text_seed_input)
 
     # 替换特定符号为空
     text = (
-        text.replace("\n", "")
+        text.replace("\n\n", ",")
         .replace(":", ",")
         .replace("：", ",")
         .replace("[", ",")
@@ -53,6 +61,8 @@ def generate_audio(
 
     print("文本长度:" + str(len(text)) + "\n")
 
+    merged_audio = np.array([])
+
     if refine_text_flag:
         if len(text) > cut_word_num:
             # 分段处理文本
@@ -60,7 +70,7 @@ def generate_audio(
                 text[i : i + cut_word_num] for i in range(0, len(text), cut_word_num)
             ]
             total_segments = len(segments)
-            generated_text = []
+            # generated_wav = []
             for i, segment in enumerate(segments):
                 print(
                     "正在处理第{}个片段（共{}个）：{}\n".format(
@@ -75,9 +85,19 @@ def generate_audio(
                     params_infer_code=params_infer_code,
                     do_text_normalization=False,
                 )
-                generated_text.append(inferred_text)
-                print("文本:" + str(generated_text) + "\n")
-            text = generated_text
+                wav = chat.infer(
+                    inferred_text,
+                    skip_refine_text=True,
+                    params_refine_text=params_refine_text,
+                    params_infer_code=params_infer_code,
+                    do_text_normalization=False,
+                )
+
+                audio_data = np.array(wav[0]).flatten()
+                merged_audio = np.append(merged_audio, audio_data)
+
+                print("文本:" + str(inferred_text) + "\n")
+
         else:
             text = chat.infer(
                 text,
@@ -87,47 +107,30 @@ def generate_audio(
                 params_infer_code=params_infer_code,
                 do_text_normalization=False,
             )
-    print(type(text))
-    text = ["".join(sublist) for sublist in text]
-    print(type(text))
-    print("\n\n文本:" + str(text) + "\n")
-
-    if len(text) > cut_word_num:
-        # 分段处理文本
-        segments = [
-            text[i : i + cut_word_num] for i in range(0, len(text), cut_word_num)
-        ]
-        total_segments = len(segments)
-        generated_audio = []
-        for i, segment in enumerate(segments):
-            print(
-                "正在处理第{}个片段（共{}个）：{}\n".format(i, total_segments, segment)
-            )
             wav = chat.infer(
-                segment,
+                text,
                 skip_refine_text=True,
                 params_refine_text=params_refine_text,
                 params_infer_code=params_infer_code,
                 do_text_normalization=False,
             )
-            audio_data = np.array(wav[0]).flatten()
-            generated_audio.append(audio_data)
-        audio_data = np.concatenate(generated_audio)
-    else:
-        wav = chat.infer(
-            text,
-            skip_refine_text=True,
-            params_refine_text=params_refine_text,
-            params_infer_code=params_infer_code,
-            do_text_normalization=False,
-        )
-        audio_data = np.array(wav[0]).flatten()
+            merged_audio = np.array(wav[0]).flatten()
+    # print(type(text))
+    # text = ["".join(sublist) for sublist in text]
+    # text = ["".join(text)]
+    # print(type(text))
+    # print("\n\n文本:" + str(text) + "\n")
 
+    # print(type(audio_data))
     # audio_data = np.array(wav[0]).flatten()
     sample_rate = 24000
     text_data = text[0] if isinstance(text, list) else text
 
-    return [(sample_rate, audio_data), text_data]
+    return [(sample_rate, merged_audio), text_data]
+
+
+def on_text_change(text_input):
+    return str(len(text_input))
 
 
 def main():
@@ -167,12 +170,22 @@ def main():
             generate_audio_seed = gr.Button("\U0001F3B2")
             text_seed_input = gr.Number(value=42, label="Text Seed")
             generate_text_seed = gr.Button("\U0001F3B2")
+            total_text_number_box = gr.TextArea(
+                lines=1,
+                label="字符数",
+                value=str(len(text_input.value)),
+            )
+            default_par = "[oral_2][laugh_0][break_6]"
+            para_box = gr.Textbox(value=default_par)
+
+        text_input.change(
+            on_text_change, inputs=text_input, outputs=total_text_number_box
+        )
 
         generate_button = gr.Button("Generate")
 
         text_output = gr.Textbox(label="Output Text", interactive=False)
         audio_output = gr.Audio(label="Output Audio")
-
         generate_audio_seed.click(generate_seed, inputs=[], outputs=audio_seed_input)
 
         generate_text_seed.click(generate_seed, inputs=[], outputs=text_seed_input)
@@ -187,6 +200,7 @@ def main():
                 audio_seed_input,
                 text_seed_input,
                 refine_text_checkbox,
+                para_box,
             ],
             outputs=[audio_output, text_output],
         )
@@ -196,9 +210,6 @@ def main():
         "--server_name", type=str, default="0.0.0.0", help="Server name"
     )
     parser.add_argument("--server_port", type=int, default=8080, help="Server port")
-    parser.add_argument(
-        "--local_path", type=str, default=None, help="the local_path if need"
-    )
     args = parser.parse_args()
 
     print("loading ChatTTS model...")
